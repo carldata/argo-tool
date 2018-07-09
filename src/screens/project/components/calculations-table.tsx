82a9e528aa1513ce9bd756071e9017468b67381c
AzureAD\RafalPompecki <rafal@carlsolutions.com>
2018-07-09 14:52:57 +0200

    clean up

--- src/screens/project/components/calculations-table.tsx
+++ src/screens/project/components/calculations-table.tsx
@@ -1,9 +1,6 @@
 import * as React from 'react';
 import { ICalculations } from '@screens/project/models';
-import * as ReactDataGrid from 'react-data-grid';
 import { DataGrid } from '@components/data-grid';
-import { D3LegendGridLayout } from '@components/auxiliary/d3-legend-grid-layout';
-import { bindActionCreators } from '../../../../node_modules/redux';
 
 export interface ICalculationsTableProps {
   calculations: ICalculations[];
@@ -33,7 +30,7 @@ export class CalculationsTable extends React.Component<ICalculationsTableProps> 
             id='next'
             type='button'
             className='btn btn-primary'
-            onClick={(e) => { alert('going to next screen, but check if row selected'); }}>
+            onClick={() => { alert('going to next screen, but check if row selected'); }}>
             Next Screen
           </button>
         </div>
