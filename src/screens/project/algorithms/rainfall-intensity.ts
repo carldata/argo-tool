af0c4fb90c76fdec28456da0cbe76c59abc9decd
Tomasz Pichlak <tpichlak@gmail.com>
2018-07-11 09:53:49 +0200

    Made rainfall intensity function and test for it.

--- src/screens/project/algorithms/rainfall-intensity.ts
+++ src/screens/project/algorithms/rainfall-intensity.ts
@@ -1,8 +1,21 @@
 import * as _ from 'lodash';
 import { ITimeSeries } from '../models/index';
 import { convertCsvStringToTimeSeries } from './auxiliary';
+import { IUnixTimePoint } from '../../../../node_modules/time-series-scroller';
 
-export const calculateRainfallIntensity = (rainfall: ITimeSeries|string): number => {
+export const calculateRainfallIntensity = (rainfall: ITimeSeries | string): number => {
   const csvRows = _.isString(rainfall) ? convertCsvStringToTimeSeries(rainfall) : rainfall;
-  return -1;
+  const duration1h: number = 60 * 60 * 1000;
+  return _.chain(csvRows)
+    .map((el, index, arr) => _.sum(window(index, duration1h, arr)))
+    .max()
+    .value();
+};
+
+const window = (index: number, duration: number, arr: IUnixTimePoint[]): number[] => {
+  return _.chain(arr)
+    .slice(0, index + 1)
+    .filter((el) => el.unix >= (arr[index].unix - duration))
+    .map((el) => el.value)
+    .value();
 };
