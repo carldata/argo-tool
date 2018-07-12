4c7f59b97a165b329012bc2a48470522467c0d08
sharp.coding <sharp.coding@gmail.com>
2018-07-12 06:54:22 +0200

    finishing Issue_#16_Prediction_error_algorithm_with_tests

--- src/screens/project/algorithms/prediction-error.ts
+++ src/screens/project/algorithms/prediction-error.ts
@@ -5,6 +5,29 @@ import { convertCsvStringToTimeSeries } from './auxiliary';
 
 export const calculatePredictionError = (flow: ITimeSeries|string, predictions: ITimeSeries|string): number => {
   const flowRows = _.isString(flow) ? convertCsvStringToTimeSeries(flow) : flow;
-  const predictionRows = _.isString(predictions) ? convertCsvStringToTimeSeries(predictions) : predictions;
-  return -1;
+  const predictionRows = _.isString(predictions) ? convertCsvStringToTimeSeries(predictions, 'time', 'mean') : predictions;
+  const flowMap: Map<number, number> = _.reduce(flowRows, (map, el) => {
+    map.set(el.unix, el.value);
+    return map;
+  }, new Map<number, number>());
+  const predictionMap: Map<number, number> = _.reduce(predictionRows, (map, el) => {
+    map.set(el.unix, el.value);
+    return map;
+  }, new Map<number, number>());
+  const mape = Array // as defined by https://en.wikipedia.org/wiki/Mean_absolute_percentage_error
+    .from(flowMap.keys())
+    .reduce((acc, k) => {
+      if (predictionMap.has(k)) {
+        return {
+          sum: acc.sum + Math.abs((flowMap.get(k) - predictionMap.get(k)) / flowMap.get(k)),
+          n: acc.n + 1,
+        };
+      }
+      return acc;
+    }, {
+      sum: 0, // sum of abs(A(t) - F(t))/A(t)
+      n: 0, // number of t's,
+    });
+
+  return mape.n > 0 ? 100.0 * mape.sum / mape.n : -1;
 };
