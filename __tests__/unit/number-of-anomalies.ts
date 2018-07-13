7422138cbf63b764cea609faad3e5cf06f005a15
Tomasz Pichlak <tpichlak@gmail.com>
2018-07-13 07:17:38 +0200

    Fixing number of anomalies test

--- __tests__/unit/number-of-anomalies.ts
+++ __tests__/unit/number-of-anomalies.ts
@@ -2,13 +2,13 @@ import * as _ from 'lodash';
 import axios from 'axios';
 import { calculateNumberOfAnomalies } from '../../src/screens/project/algorithms/number-of-anomalies';
 
-describe('calcluate number of anomalies', () => {
+describe('calculate number of anomalies', () => {
   it.only('testing with 7881-11742 for 2018-07-05', (done) => {
     axios
       .get('http://localhost:3900/static-csv/7881-11742.anomalies.csv')
       .then((anomalies) => {
         const numberOfAnomalies = calculateNumberOfAnomalies(anomalies.data);
-        expect(numberOfAnomalies).toEqual(288);
+        expect(numberOfAnomalies).toEqual(287);
         done();
       });
   });
